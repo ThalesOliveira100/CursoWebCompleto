fbecac5e0d1be7dd4bf5def68cb162099bbd428a
Thales Oliveira <TOliveiraDev@outlook.com>
2024-01-18 22:40:05 -0300

    resolvido erro de BodyParser, incluido metodo PUT e DELETE

--- NODE/PROJETO/src/servidor.js
+++ NODE/PROJETO/src/servidor.js
@@ -3,7 +3,11 @@ const porta = 3003;
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const db = require('./db')
+
+// Verifica envio das informações do body, e se elas estiverem com o formato urlEncoded, ele irá realizar o Parser, transformando os dados em Objeto.
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // app.use == sempre irá conseguir acessar o middleware informado, independente do caminho.
 
@@ -21,10 +25,24 @@ app.get('/produtos/:id', (req, res, next) => {
 // Deve ser enviado utilizando o metodo POST, escrito em um body com a opção 'x-www-form-urlencoded' selecionada.
 app.post('/produtos', (req, res, next) => {
     const produto = db.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     });
     res.send(produto); // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = db.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+    res.send(produto); // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = db.excluirProdutos(req.params.id);
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
